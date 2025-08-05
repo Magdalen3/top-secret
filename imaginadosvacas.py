eb7235a7df86630990fea6a85760c06a202d217f
magdalaptop <delriomagdalene@gmail.com>
2025-08-05 00:32:56 -0300

    resize

--- imaginadosvacas.py
+++ imaginadosvacas.py
@@ -5,14 +5,24 @@ woof = 600
 
 with Image.open("betin.jpg") as img:
     og_bark, og_woof = img.size
+
+    #reducir tamaño, solo para obtener color, no se usará más esa variable
     if (og_woof > 600):
         bark = (og_bark * woof) // og_woof
-        img=img.resize((bark, woof))
+        imgresize=img.resize((bark, woof))
+    
+    #obtener color más usado
+    imgforcolor = imgresize.quantize(colors=256).convert("RGB")
+    colors = imgforcolor.getcolors()
+    obtenercolor = sorted(colors, reverse=True)
+    obtenercolor = obtenercolor[0][1]
+
+    #escala de grises solo para tesseract
+    imgris=img.convert("L")
+
+
     #img.show()
-    img = img.quantize(colors=256)  
-    colors = img.getcolors(maxcolors=256)
-    img.show()
-    #print (colors)
+    #print (obtenercolor)
 
 
 
